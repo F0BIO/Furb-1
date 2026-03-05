6c5032823602d270add2611faeb54bab0afde66c
Fábio Henrique Couto <137837507+F0BIO@users.noreply.github.com>
2026-03-04 21:49:11 -0300

    Add files via upload

---
+++ Atividade2/Atividade2/src/App.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class App {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Funcionario funcionario = new Funcionario(" ", 0);
+            
+        double[] funcionarioIrpf;
+        funcionarioIrpf = new double[5];
+        String[] funcionarioNome;
+        funcionarioNome = new String[5];
+        
+        for (int i = 0; i < funcionarioIrpf.length; i++) {
+            System.out.print("Imforme seu nome: ");
+            funcionario.setNome(scanner.next());
+
+            System.out.print("Imforme o salário do funcionário: ");
+            funcionario.setSalario(scanner.nextDouble());
+
+            funcionarioNome[i] = funcionario.getNome();
+            funcionarioIrpf[i] = funcionario.calcularIrpf();
+        }
+        for (int i = 0; i < funcionarioIrpf.length; i++) {
+            System.out.println("Nome: " + funcionarioNome[i]);
+            System.out.printf("O Total de impostos a serem pagos são R$ %.2f \n", funcionarioIrpf[i]);
+        }
+        scanner.close();
+    }
+}
